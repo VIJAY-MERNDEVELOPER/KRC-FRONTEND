3dbf1b2a8924c406010b2c72787f3e15b6bc547e
VIJAY-MERNDEVELOPER <142765666+VIJAY-MERNDEVELOPER@users.noreply.github.com>
2024-04-30 12:29:23 +0530

    commit changes 1

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import Recipe from "./pages/Recipe";
 
 import UserManagement from "./pages/UserManagement";
 
-axios.defaults.baseURL = "http://localhost:3001/api";
+axios.defaults.baseURL = "https://krc-backend.onrender.com/api";
 // "http://localhost:3001/api";
 // axios.defaults.withCredentials = true;
 // "https://krc-backend.onrender.com/api";
